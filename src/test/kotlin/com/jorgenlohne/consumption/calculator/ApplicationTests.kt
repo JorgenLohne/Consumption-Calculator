8cb990eb0ce1ef43bdf11278ebed2421ac6d92bc
Jørgen Lohne <jorgen.lohne@tietoevry.com>
2025-05-04 16:05:02 +0200

    inital commit

---
+++ src/test/kotlin/com/jorgenlohne/consumption/calculator/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jorgenlohne.consumption.calculator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
